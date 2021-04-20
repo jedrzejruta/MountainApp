7e2525b9c9347ebfd17375bc864e39927c916ad2
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2021-04-20 22:21:29 +0200

    upgrade to expo sdk 41

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, SafeAreaView} from 'react-native';
-import { ProfileScreenNavProp } from '../../components/DrawerNavigation';
+import { ProfileScreenNavProp } from '../components/DrawerNavigation';
 
 type ProfileScreenProps = {
 	navigation: ProfileScreenNavProp;
